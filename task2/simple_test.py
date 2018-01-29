2c2da62d1411f43186d1eb689ec0dc00f472ac5f
Siyan <sw2936@columbia.edu>
2018-01-28 19:50:29 -0500

    exp1

---
+++ task2/simple_test.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pytest
+import io
+
+def test_built_in_functiton():
+    #test using built-in function
+    np.testing.assert_allclose(2.0/8.0,0.25,rtol=1e-5)
+    
+def test_array():
+    #test using numpy.array
+    np.testing.assert_allclose(np.divide(2.0,8.0),0.25,rtol=1e-5)
+    
+
+def test_input_size():
+    f=open("/Users/admin/desktop/applied machine learning/applied-ml-hw/task2/input.txt","rb")
+    char=f.readline().strip().decode("utf-8")
+    assert len(char)==6
